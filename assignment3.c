aecb0e0b96ecb93de3ef763624e230eebe70ccf0
James Walsh <jamesgcwalsh@gmail.com>
2020-10-22 14:27:23 -0400

    added error check for thread creation

--- assignment3.c
+++ assignment3.c
@@ -1,6 +1,5 @@
 /* 	CS3305A 	Assignment 3
-	James Walsh	jwalsh57	250481718	
-*/
+	James Walsh	jwalsh57	250481718 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,14 +9,16 @@
 #include <errno.h>
 #include <pthread.h>
 
-int x, y, z; 
+int x, y, z;
 
-void *sum(){
+void *sum()
+{
 	z=y+x;
 	return NULL;
 }
 
-int main(){
+int main()
+{
 	pid_t child, forking_pid;
 	pthread_t thread;
 
@@ -28,32 +29,35 @@ int main(){
     */
 
    /* Add code under every comment to demonstrate differences between child and thread */
-   
+
 	forking_pid = fork(); // create child process
-	
+
 	if (forking_pid<0) // check for child creation error
 	 {
 		 printf("fork unsuccessful\n");
-		 printf("main function: errno number is %d\n", errno);	
+		 printf("main function: errno number is %d\n", errno);
 		 exit(1);
 	 }
 
 	if(forking_pid>0) // in parent:
 	{
-	wait(NULL); // wait for child, and print value of z
-	printf("Using a fork(), the value of z in the parent process is: %d\n", z); //value of z after the fork process won't change
-	
-	// create thread, wait for it to complete, then print value of z
-	pthread_create(&thread, NULL, sum, NULL);
-	pthread_join(thread, NULL);
-	printf("Using a thread, the value of z is: %d\n", z); //value of z after passing to the thread will change
+		wait(NULL); // wait for child, and print value of z
+		printf("Using a fork(), the value of z in the parent process is: %d\n", z); //value of z after the fork process won't change
 
+	// create thread, wait for it to complete, then print value of z
+		if (pthread_create(&thread, NULL, sum, NULL))
+		{
+			printf("Thread creation unsuccessful.\n");
+			exit(1);
+		}
+		pthread_join(thread, NULL);
+		printf("Using a thread, the value of z is: %d\n", z); //value of z after passing to the thread will change
 	}
 
 	if(forking_pid==0)// in child:
 	{
-	sum();// sum x and y and store it in z
-	printf("Using a fork(), the value of z in the child process is: %d\n", z); // print value of z
+		sum();// sum x and y and store it in z
+		printf("Using a fork(), the value of z in the child process is: %d\n", z); // print value of z
 	}
 
 	return 0;
